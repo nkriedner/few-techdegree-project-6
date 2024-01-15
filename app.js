24049e3ae1b3476e7b24e635a104373fb7f654fb
Nils Kriedner <nkriedner@googlemail.com>
2024-01-15 02:13:52 +0100

    Create getRandomPhrasesArray function

--- app.js
+++ app.js
@@ -7,8 +7,19 @@ const startGameBtn = document.querySelector(".btn__reset");
 const phrases = ["Sesame", "Abrakadabra", "Simsalabim", "Hokuspokus", "I am happiness"];
 let missed = 0;
 
+// FUNCTIONS
+function getRandomPhraseArray(arr) {
+    const randomNumber = Math.floor(Math.random() * arr.length);
+    const randomPhrase = arr[randomNumber];
+    const randomPhaseSplit = randomPhrase.split("");
+    return randomPhaseSplit;
+}
+
 // EVENT HANDLERS
 startGameBtn.addEventListener("click", () => {
     // Hide the start screen overlay
     document.getElementById("overlay").style.display = "none";
 });
+
+// NEXT STEP(S)
+// -> Create a getRandomPhraseArray function
