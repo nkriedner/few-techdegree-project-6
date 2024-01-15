3f5d672e89c149fbfa2e512e07982eec17eb7484
Nils Kriedner <nkriedner@googlemail.com>
2024-01-15 01:58:42 +0100

    Hide overlay when start button is clicked

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 // SELECT ELEMENTS
 const keyboard = document.getElementById("qwerty");
 const phraseContainer = document.getElementById("phrase");
+const startGameBtn = document.querySelector(".btn__reset");
 
 // DEFINE VARIABLES
 let missed = 0;
+
+// EVENT HANDLERS
+startGameBtn.addEventListener("click", () => {
+    // Hide the start screen overlay
+    document.getElementById("overlay").style.display = "none";
+});
